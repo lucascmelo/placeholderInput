625fad09f27ac1b42dc6038b5096770f3be38255
Lucas Cavalcanti Melo <lucas.cavalcanti.melo@gmail.com>
2013-06-09 11:54:48 -0300

    #fix - Adicionando valor do placeholder no value lobo que inicia o plugin; #fix - Suporte para IE8 - mudando o type para o setAttribute;

--- placeholderinput_dev.js
+++ placeholderinput_dev.js
@@ -4,7 +4,7 @@ Lucas Cavalcanti (http://lucascavalcanti.com.br)
 License: http://creativecommons.org/licenses/by/2.5/
 ================================================================
 How to use: Add attribute placeholder in input and textarea
-Example: 
+Example:
 <input type="text" placeholder="Enter your name">
 <input type="text" placeholder="Enter your email">
 <textarea name="" id="" placeholder="Type your message"></textarea>
@@ -22,34 +22,31 @@ function placeholderInput(){
 		textarea = document.getElementsByTagName("textarea"),
 		maxInputs = input.length + textarea.length;
 
-
-
-
 	for (var i = 0; i < maxInputs; i++) {
 		if (input[i]!=undefined) {
 			inputVal = input[i].getAttribute('placeholder');
-			
+			input[i].value = inputVal;
 			if (input[i].type=='password'){
-				input[i].type = 'text';
+				input[i].setAttribute('type','text');
 				input[i].setAttribute('data-type','pass');
 			};
 
 			input[i].onfocus = function(){
 				if (this.getAttribute('data-type')=='pass'){
-					this.type = 'password';
+					this.setAttribute('type','password');
 				};
-				
+
 				if (this.value=="" || this.value == this.getAttribute("placeholder")){
-					this.value='';	
+					this.value='';
 				};
 			};
-			
+
 			input[i].onblur = function(){
 				if (this.value == "" || this.value == this.getAttribute('placeholder') ) {
 					this.value = this.getAttribute('placeholder');
-					this.type = 'text';
+					this.setAttribute('type','text');
 				};
-				
+
 			};
 		};
 		if(textarea[i]!=undefined){
@@ -62,7 +59,7 @@ function placeholderInput(){
 
 			textarea[i].onblur = function(){
 				if (this.value=="" || this.value== this.getAttribute('placeholder') ) {
-					this.value = this.getAttribute('placeholder');	
+					this.value = this.getAttribute('placeholder');
 				};
 			};
 		};
